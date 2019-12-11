8c12d730729386a05622da42eeeaf6f2ed4efdd8
billalp <billal.patel@rightmove.co.uk>
2019-12-11 11:32:08 +0000

    Added dummy db for now

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 const express = require('express');
 
+const app = express();
+const port = 5000;
+const db = {
+    users: [
+        {
+            id: 1,
+            name: 'Bill',
+            email: 'bill@bill.com',
+            password: 'password1',
+            joined: new Date().getDate
+        },
+        {
+            id: 2,
+            name: 'Bob',
+            email: 'bob@bob.com',
+            password: 'password2',
+            joined: new Date().getDate
+        }
+    ]
+}
+
+app.listen(port, () => console.log(`Express server started on port ${port}`));
+
+app.get('/', (req, res) => {
+    res.send('landing');
+})
+
+app.post('/signin', (req, res) => {
+    res.json('post');
+})
+
+// app.get('/', (req, res) => res.send('Hello World!'))
